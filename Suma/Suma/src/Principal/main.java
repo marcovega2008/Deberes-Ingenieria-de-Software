764a6d8344578fd97f9760d5b7b21a4ef249baf0
MARCO VEGA <marquito.quito@hotmail.com>
2018-01-02 20:50:12 -0500

    Update main.java

--- Suma/Suma/src/Principal/main.java
+++ Suma/Suma/src/Principal/main.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Principal;
 
 import sumar.suma;
 
-/**
- *
- * @author David
- */
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         int a=2,b=3,resultado;
         suma sum = new suma();
